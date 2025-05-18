ad9b26de320b20957752e1a6cdc6ed928804d92f
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-05-18 17:59:14 +0530

    Typecasting

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+print("Before type casting")
+a = 26
+print(a,type(a))
+b = "Aarush"
+print(b,type(b))
+c = True
+print(c,type(c))
+d = 13.96
+print(d,type(d))
+print("After type casting")
+a = float(a)
+print(a,type(a))
+c = str(c)
+print(c,type(c))
+d = int(d)
+print(d,type(d))
+a = str(a)
+print(a,type(a))
